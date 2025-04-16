4d0242bddd8cc7cecadefea797dda2b1fbaa4e92
Ba Yin Min <26690979+bayinmin@users.noreply.github.com>
2025-04-16 15:18:57 -0400

    Add files via upload

--- find-jwks-private-keys.py
+++ find-jwks-private-keys.py
@@ -5,8 +5,12 @@ def validate_jwks(file_path):
         jwks = json.load(f)
     
     for key in jwks.get("keys", []):
-        if "d" in key:  # Private key component for RSA/EC
-            print("Private key exposed: ", key)
+        sensitive_fields = ["d", "p", "q"]  # Private key components
+        exposed_fields = [field for field in sensitive_fields if field in key]
+
+        if exposed_fields:
+            print(f"Private key components exposed: {exposed_fields}")
+            print("Exposed Key: ", key)
             return False
     print("No private keys detected. JWKS is safe.")
     return True
